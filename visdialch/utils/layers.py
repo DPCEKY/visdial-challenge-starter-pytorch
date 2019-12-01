b3f194175939b7f72646c0f67224e5d0799dc2c0
Zhihao Zhou <zhouzhihao90@gmail.com>
2019-11-30 22:51:23 -0500

    Update layers.py

--- visdialch/utils/layers.py
+++ visdialch/utils/layers.py
@@ -50,7 +50,7 @@ class QUES_KVQ(nn.Module):
         kv = self.kv(ques_word_embed)
         q = self.q(ques_word_embed)
         att = self.att(ques_word_encoded).squeeze(-1) + float('-inf') * (1 - ques_not_pad)
-        att = F.softmax(att).unsqueeze(-1)
+        att = F.softmax(att, dim=-1).unsqueeze(-1)
 
         print(att)
 
