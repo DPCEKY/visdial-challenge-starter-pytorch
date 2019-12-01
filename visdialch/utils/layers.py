e9d70387e04a8549bd180e53c0548e1464fb649a
Zhihao Zhou <zhouzhihao90@gmail.com>
2019-11-30 22:43:34 -0500

    Update layers.py

--- visdialch/utils/layers.py
+++ visdialch/utils/layers.py
@@ -42,8 +42,10 @@ class QUES_KVQ(nn.Module):
         )
 
     def forward(self, ques_word_embed, ques_lens, ques_not_pad):
+        num_rounds = ques_word_embed.size(1)
         ques_word_embed = ques_word_embed.view([-1, ques_word_embed.size(-2), ques_word_embed.size(-1)])  # (batch_size * num_rounds, que_len_max, word_embed_size)
         ques_word_encoded, _ = self.ques_rnn(ques_word_embed, ques_lens)  # shape: (batch_size*num_rounds, quen_len_max, lstm_hidden_size*2)
+        ques_word_encoded = ques_word_encoded.view([-1, num_rounds, ques_word_encoded.size(-2), ques_word_encoded.size(-1)])
 
         kv = self.kv(ques_word_embed)
         q = self.q(ques_word_embed)
